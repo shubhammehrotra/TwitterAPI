0fee53e028d8fbb0f42e40a1693a3a08a8fd8c10
shubhammehrotra94 <shubhammehrotra94@gmail.com>
2016-03-21 17:46:32 +0000

    V1

---
+++ tweet_dumper.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+
+#Twitter API credentials
+consumer_key = "FZUx3R6dqOkyQv6Kzd4swr1yi"
+consumer_secret = "xJGrrQflj6KjBS5oNKFj0s4NeE0vbWCIKxUqQO9RFB6U715Jog"
+access_key = "41558223-fXQwbhvXB9gOKM3wWNZzmhWT8GTKiDDFIKVvi9w2V"
+access_secret = "mqc2lcaWzWgatJaMKgx4JmP6knKucLMbIAf3BEZqFIvnh"
+
+
+def get_all_tweets(user_id):
+	#Twitter only allows access to a users most recent 3240 tweets with this method
+	
+	#authorize twitter, initialize tweepy
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+	
+	#initialize a list to hold all the tweepy Tweets
+	alltweets = []	
+	
+	#make initial request for most recent tweets (200 is the maximum allowed count)
+	new_tweets = api.user_timeline(user_id = user_id,count=200)
+	
+	#save most recent tweets
+	alltweets.extend(new_tweets)
+	
+	#save the id of the oldest tweet less one
+	oldest = alltweets[-1].id - 1
+	
+	#keep grabbing tweets until there are no tweets left to grab
+	while len(new_tweets) > 0:
+		print ("getting tweets before %s" % (oldest))
+		
+		#all subsiquent requests use the max_id param to prevent duplicates
+		new_tweets = api.user_timeline(user_id = user_id,count=200,max_id=oldest)
+		
+		#save most recent tweets
+		alltweets.extend(new_tweets)
+		
+		#update the id of the oldest tweet less one
+		oldest = alltweets[-1].id - 1
+		
+		print ("...%s tweets downloaded so far" % (len(alltweets)))
+	
+	#transform the tweepy tweets into a 2D array that will populate the csv	
+	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+	
+	#write the csv	
+	with open('%s_tweets.csv' % user_id, 'w',newline='') as f:
+		writer = csv.writer(f)
+		writer.writerow(["id","created_at","text"])
+		writer.writerows(outtweets)
+	
+	pass
+
+
+if __name__ == '__main__':
+	#pass in the username of the account you want to download
+        userid = ['33507131','92521829','238158030','84546428','20586381','32845471','74979783','245320407','236108605','54172831']
+        for i in range(len(userid)):
+                get_all_tweets(userid[i])
